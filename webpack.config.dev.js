a810a0560c8bcd4586579fcf35b8aea5406df5a1
Mariusz Baran <mariusz.baran23@gmail.com>
2018-07-27 23:19:09 +0200

    Initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, '/src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.jsx?$/,
+        include: path.join(__dirname, 'src'),
+        query: {
+          presets: ['es2015', 'react', 'stage-0']
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      components: path.resolve('src/components'),
+      containers: path.resolve('src/containers')
+    },
+    extensions: ['.js']
+  },
+  mode: 'development'
+}
