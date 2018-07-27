fce9412113c6f5b178118b011273ac9b426c1057
Mariusz Baran <mariusz.baran23@gmail.com>
2018-07-28 00:57:51 +0200

    Create cards and todos constants for redux actions

---
+++ src/actions/constants/cardsConstants.js
@@ -0,0 +1,6 @@
+// Cards constants
+export const CREATE_CARD = 'CREATE_CARD';
+export const UPDATE_CARD = 'UPDATE_CARD';
+export const DELETE_CARD = 'DELETE_CARD';
+export const ADD_TODO = 'ADD_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
---
+++ src/actions/constants/todosConstants.js
@@ -0,0 +1,5 @@
+// Todos constants
+export const CREATE_TODO = 'CREATE_TODO';
+export const UPDATE_TODO = 'UPDATE_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const DELETE_TODOS = 'DELETE_TODOS';
