0ac8e8bbac79c1b9e1e710abe9959472bb2fe192
Mariusz Baran <mariusz.baran23@gmail.com>
2018-07-28 14:22:51 +0200

    Create cards and todos List components

---
+++ src/components/CardsList/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const CardsList = styled.ul`
+  padding: 1rem;
+  list-style-type: none;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 300px);
+  grid-column-gap: 2rem;
+  grid-row-gap: 2rem;
+`
+
+export default CardsList;
---
+++ src/components/TodosList/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const TodosList = styled.ul`
+  list-style-type: none;
+  padding: .5rem;
+`
+
+export default TodosList;
